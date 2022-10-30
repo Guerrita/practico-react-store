dcb7724ea26c42527fd4f21d2c60b203aef0c3e2
Guerrita <53787821+Guerrita@users.noreply.github.com>
2022-10-30 18:13:54 -0500

    Se inicia una nueva seccion en el home y se comienza la creacion del footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "@styles/Footer.scss";
+
+// import banner from "@logos/doctores.png";
+
+const Footer = () => {
+	return (
+		<>
+			<section className='footer'>
+				<div>
+					<h5>Con este sistema podras gestionar todo lo relacionado con el sistema de urgencias de tu centro hospitalario </h5>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,26 @@ const Home = () => {
 			<section className='banner'>
 				<div>
 					<h1 className=''>¡Bienvenido a Urgencias Rurales!</h1>
-					<h5></h5>
+					<h5>Con este sistema podras gestionar todo lo relacionado con el sistema de urgencias de tu centro hospitalario </h5>
 				</div>
 				<img src={banner} alt="doctores" className="doctores" />
-
+			</section>
+			<section className='information-cards'>
+				<article className='card'>
+					<img />
+					<h4>Pacientes</h4>
+					<p>Podrás administrar a todos los pacientes que han ingresado y que ingresarán a urgencias</p>
+				</article>
+				<article className='card'>
+					<img />
+					<h4>Médicos</h4>
+					<p>Tendrás una lista con los médicos que estan actualmente trabajando dentro del sistema de urgencias</p>
+				</article>
+				<article className='card'>
+					<img />
+					<h4>Mucho mas</h4>
+					<p></p>
+				</article>
 			</section>
 		</>
 	);
